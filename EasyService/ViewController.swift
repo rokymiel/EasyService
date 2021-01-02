6f31ac999d27b80db729c628fd22df4f4ddacace
rokymiel <rokymiel@gmail.com>
2021-01-02 21:04:40 +0300

    init commit

---
+++ EasyService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyService
+//
+//  Created by Михаил on 02.01.2021.
+//  Copyright © 2021 rokymiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
