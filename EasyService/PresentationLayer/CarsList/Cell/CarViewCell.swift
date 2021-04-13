3d6f1d44950750e436f67cd1664a5f1f452cf265
rokymiel <rokymiel@gmail.com>
2021-04-14 01:45:50 +0300

    Ячейка автомобиля

---
+++ EasyService/PresentationLayer/CarsList/Cell/CarViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarViewCell.swift
+//  EasyService
+//
+//  Created by Михаил on 14.04.2021.
+//  Copyright © 2021 rokymiel. All rights reserved.
+//
+
+import UIKit
+
+class CarViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
